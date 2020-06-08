50bc59483aee17ed961d9dba8ec8b0c9c7cbaa7d
Julian Bustos <julian.m.bustos@gmail.com>
2020-06-08 18:55:14 +0000

    added /urls.json to web

--- express_server.js
+++ express_server.js
@@ -16,3 +16,8 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
