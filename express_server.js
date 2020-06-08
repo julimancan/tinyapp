d90106222617675a2cc0163cb3ffd804cb6462c5
Julian Bustos <julian.m.bustos@gmail.com>
2020-06-08 22:34:41 +0000

    added shorturl

--- express_server.js
+++ express_server.js
@@ -27,3 +27,18 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  if (urlDatabase[req.params.shortURL]) {
+    let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+    res.render("urls_show", templateVars);
+  } else {
+    // this is a future step.
+    res.redirect("/urls")
+  }
+});
